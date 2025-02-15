f0fe15b623d8419899c52162c75e94a8c1d31085
serferna <sergioffdev@gmail.com>
2025-02-15 16:08:38 +0100

    Mejora la rutina del filósofo para manejar el estado de la primera acción y optimizar el registro de log

--- src/philosopher/philosopher_routine.c
+++ src/philosopher/philosopher_routine.c
@@ -15,13 +15,23 @@
 void	*philosopher_routine(void *philosopher)
 {
 	t_philosopher	*philo;
+	t_bool is_first_action;
 
+	is_first_action = TRUE;
 	philo = (t_philosopher *)philosopher;
 	philo->last_meal = timestamp();
 	philo->routine = philosopher_eat_state;
 	if (is_even(philo->id))
 		usleep(15000);
-	while (!philo->global->dead && !has_eaten_required_meals(philo))
+	while (!philo->global->dead)
+	{
+		if (philo->routine == philosopher_eat_state && !is_first_action)
+			print_log(philosopher, THINKING);
+		if (is_first_action)
+			is_first_action = FALSE;
+		if (has_eaten_required_meals(philosopher))
+			break;
 		philo->routine(philo);
+	}
 	return (NULL);
 }
